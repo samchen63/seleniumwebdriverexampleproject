4b220dbe46190608e26d5c7943f4e8e3a87739be
Sam Chen <samchen63@users.noreply.github.com>
2019-06-18 14:37:58 +1000

    Update HomePage.cs

--- SeleniumWebDriverFramework/Pages/HomePage.cs
+++ SeleniumWebDriverFramework/Pages/HomePage.cs
@@ -1,11 +1,3 @@
-﻿//-----------------------------------------------------------------------------
-// <copyright file="HomePage.cs" company="Planit Testing">
-//      Copyright © 2019 Planit Testing.
-//      All rights reserved.
-// </copyright>
-// <created>26/03/2019</created>
-// <author>Sam Chen</author>
-//-----------------------------------------------------------------------------
 namespace SeleniumWebDriverFramework.Pages
 {
     using OpenQA.Selenium;
